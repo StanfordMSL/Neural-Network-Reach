ea38dde75eeaf27adeba5cb388ee040da0b9233f
Joe Vincent <joevincentkc@gmail.com>
2021-07-03 16:45:54 -0700

    one more filepath change

--- load_networks.jl
+++ load_networks.jl
@@ -149,7 +149,7 @@ end
 
 # Load networks given as .mat files that were obtained from extract_onnx_params.py
 function load_mat_onnx_acas(filename)
-	vars = matread(string("Neural-Network-Reach/", filename))
+	vars = matread(string("/home/ubuntu/work/Neural-Network-Reach/", filename))
 	weight = r"MatMul_W"
 	bias = r"Add_B"
 
